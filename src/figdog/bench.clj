6f35f52e5c04e3f3b48d0de49fdc265d48529438
Hunter Kelly <retnuh@gmail.com>
2015-02-04 18:25:04 +0000

    fix dropbox sync; make tweak easier for intellij repl stuff

--- src/figdog/bench.clj
+++ src/figdog/bench.clj
@@ -6,41 +6,43 @@
     [figdog.transducer :as trans]
     [figdog.recursive :as recursive]))
 
-(println "lazy" (time (lazy/find-path "fig" "dog")))
-(quick-bench (lazy/find-path "fig" "dog"))
-(println "trans first" (time (trans/find-path "fig" "dog" trans/words-seq-first)))
-(quick-bench (trans/find-path "fig" "dog" trans/words-seq-first))
-(println "trans" (time (trans/find-path "fig" "dog" trans/words-seq)))
-(quick-bench (trans/find-path "fig" "dog" trans/words-seq))
+#_(do
+  (println "lazy" (time (lazy/find-path "fig" "dog")))
+  (quick-bench (lazy/find-path "fig" "dog"))
+  (println "trans first" (time (trans/find-path "fig" "dog" trans/words-seq-first)))
+  (quick-bench (trans/find-path "fig" "dog" trans/words-seq-first))
+  (println "trans" (time (trans/find-path "fig" "dog" trans/words-seq)))
+  (quick-bench (trans/find-path "fig" "dog" trans/words-seq))
 
 
-(println "lazy " (time (lazy/find-path "pearl" "water")))
-(quick-bench (lazy/find-path "pearl" "water"))
-(println "trans first" (time (trans/find-path "pearl" "water" trans/words-seq-first)))
-(quick-bench (trans/find-path "pearl" "water" trans/words-seq-first))
-(println "trans" (time (trans/find-path "pearl" "water" trans/words-seq)))
-(quick-bench (trans/find-path "pearl" "water" trans/words-seq))
+  (println "lazy " (time (lazy/find-path "pearl" "water")))
+  (quick-bench (lazy/find-path "pearl" "water"))
+  (println "trans first" (time (trans/find-path "pearl" "water" trans/words-seq-first)))
+  (quick-bench (trans/find-path "pearl" "water" trans/words-seq-first))
+  (println "trans" (time (trans/find-path "pearl" "water" trans/words-seq)))
+  (quick-bench (trans/find-path "pearl" "water" trans/words-seq))
 
-; recursive driver
+  ; recursive driver
 
-(println "recursive, lazy" (time (recursive/find-path "fig" "dog" lazy/make-words)))
-(quick-bench (recursive/find-path "fig" "dog" lazy/make-words))
-(println "recursive, trans first" (time (recursive/find-path "fig" "dog" trans/make-words-first)))
-(quick-bench (recursive/find-path "fig" "dog" trans/make-words-first))
-(println "recursive, trans" (time (recursive/find-path "fig" "dog"
-                                                       (let [seen (trans/seen-once)]
-                                                         #(trans/make-words %1 %2 seen)))))
-(quick-bench (recursive/find-path "fig" "dog"
-                                  (let [seen (trans/seen-once)]
-                                    #(trans/make-words %1 %2 seen))))
+  (println "recursive, lazy" (time (recursive/find-path "fig" "dog" lazy/make-words)))
+  (quick-bench (recursive/find-path "fig" "dog" lazy/make-words))
+  (println "recursive, trans first" (time (recursive/find-path "fig" "dog" trans/make-words-first)))
+  (quick-bench (recursive/find-path "fig" "dog" trans/make-words-first))
+  (println "recursive, trans" (time (recursive/find-path "fig" "dog"
+                                                         (let [seen (trans/seen-once)]
+                                                           #(trans/make-words %1 %2 seen)))))
+  (quick-bench (recursive/find-path "fig" "dog"
+                                    (let [seen (trans/seen-once)]
+                                      #(trans/make-words %1 %2 seen))))
 
-(println "recursive, lazy" (time (recursive/find-path "pearl" "water" lazy/make-words)))
-(quick-bench (recursive/find-path "pearl" "water" lazy/make-words))
-(println "recursive, trans first" (time (recursive/find-path "pearl" "water" trans/make-words-first)))
-(quick-bench (recursive/find-path "pearl" "water" trans/make-words-first))
-(println "recursive, trans" (time (recursive/find-path "pearl" "water"
-                                                       (let [seen (trans/seen-once)]
-                                                         #(trans/make-words %1 %2 seen)))))
-(quick-bench (recursive/find-path "pearl" "water"
-                                  (let [seen (trans/seen-once)]
-                                    #(trans/make-words %1 %2 seen))))
+  (println "recursive, lazy" (time (recursive/find-path "pearl" "water" lazy/make-words)))
+  (quick-bench (recursive/find-path "pearl" "water" lazy/make-words))
+  (println "recursive, trans first" (time (recursive/find-path "pearl" "water" trans/make-words-first)))
+  (quick-bench (recursive/find-path "pearl" "water" trans/make-words-first))
+  (println "recursive, trans" (time (recursive/find-path "pearl" "water"
+                                                         (let [seen (trans/seen-once)]
+                                                           #(trans/make-words %1 %2 seen)))))
+  (quick-bench (recursive/find-path "pearl" "water"
+                                    (let [seen (trans/seen-once)]
+                                      #(trans/make-words %1 %2 seen))))
+  )
