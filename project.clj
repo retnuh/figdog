e8ebb57a33e534c80a0c79bec17fa8366998fb8a
Hunter Kelly <retnuh@gmail.com>
2015-02-04 15:07:39 +0000

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject figdog "0.1.0-SNAPSHOT"
+  :description "various implementations of fig-fog-dog problem"
+  :license {:name "The Unlicense"
+            :url "http://unlicense.org/UNLICENSE"}
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [criterium "0.4.3"]])
