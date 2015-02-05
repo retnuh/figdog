97349d4c6338f567e2c21ef65aa3f9acb4902d82
Hunter Kelly <retnuh@gmail.com>
2015-02-05 11:22:05 +0000

    tweaks; update alpha version

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "various implementations of fig-fog-dog problem"
   :license {:name "The Unlicense"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [criterium "0.4.3"]])
